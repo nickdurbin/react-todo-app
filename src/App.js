ad6bb1d334cafc54218de1755a1a36ff58db8bfd
ndurbin <nick.durbin@gmail.com>
2019-06-30 00:27:15 -0400

    Created the add functionality to the todo list app with a submit button.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo'
 
 class App extends Component {
   state = {
@@ -41,8 +42,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos 
+          todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+        </div>
       </div>
     );
   }
