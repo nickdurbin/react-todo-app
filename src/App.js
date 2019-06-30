6e52bc727775d6c32ebe56a02d96226a48ee692a
ndurbin <nick.durbin@gmail.com>
2019-06-29 22:24:22 -0400

    Created the state object and some props/components for todos and todo items with a little bit of styling.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>To-Do</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Wireframe project',
+        completed: false
+      },
+      {
+      id: 2,
+      title: 'Build application',
+      completed: false
+      },
+      {
+      id: 3,
+      title: 'Make UI/UX clean',
+      completed: false
+      }
+    ]
+  }
+
+  markComplete = (id) => { 
+    this.setState({todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })})
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete}/>
+      </div>
+    );
+  }
 }
 
 export default App;
+;
