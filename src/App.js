4962ac1f3cffe7c1b5e21245338bca702f70b187
ndurbin <nick.durbin@gmail.com>
2019-06-30 02:26:28 -0400

    Made some style changes and compiled the app for deployment.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=3')
       .then(res => this.setState({ todos: res.data }))
   }
 
