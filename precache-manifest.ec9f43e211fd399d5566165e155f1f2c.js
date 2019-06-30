82408ea72f0057dc5c9ad4a9cf31adf4662e7d20
ndurbin <nick.durbin@gmail.com>
2019-06-30 03:33:46 -0400

    Updates

---
+++ precache-manifest.ec9f43e211fd399d5566165e155f1f2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b448599c4acc4e6910eb77f269bd7777",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "34ac35dc654fc32a0227",
+    "url": "/react-todo-app/static/css/main.8d3170fe.chunk.css"
+  },
+  {
+    "revision": "5eea9cc817ad68512938",
+    "url": "/react-todo-app/static/js/2.99f4aa59.chunk.js"
+  },
+  {
+    "revision": "34ac35dc654fc32a0227",
+    "url": "/react-todo-app/static/js/main.d1ce5605.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  }
+]);
